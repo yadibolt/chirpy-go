70f357f3867e87c70ede331c839fac4cd87afc5d
Martin Porubčan <root@martindt.localdomain>
2025-09-16 22:12:17 +0200

    add: new handler for assets

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	
 
 	server := &http.Server{
 		Addr: ":" + "8080",
