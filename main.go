0509c428dfdc74ec3994fea8fcdb499e8b10a9ab
Martin Porubčan <root@martindt.localdomain>
2025-09-17 22:31:59 +0200

    add: new endpoints, handle global state

--- main.go
+++ main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	FServerHits atomic.Int32
+}
+
+
+func (config *apiConfig) metricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		config.FServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
-	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+
+	// main entry
+	fileServerAppHandler := http.StripPrefix("/app/", http.FileServer(http.Dir("./app/")))
+	mux.Handle("/app/", apiCfg.metricsMiddleware(fileServerAppHandler))
+	
+	// assets
+	fileServerAssetsHandler := http.StripPrefix("/app/assets", http.FileServer(http.Dir("./app/assets/")))
+	mux.Handle("/app/assets/", fileServerAssetsHandler)
+
+	// server health
+	mux.HandleFunc("/healthz/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	// metrics - hits
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		hits := fmt.Sprintf("Hits: %d", apiCfg.FServerHits.Load())
+		
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(hits))
+	})
+
+	// metrics - reset
+	mux.HandleFunc("/reset/", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.FServerHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+	})
 	
 
 	server := &http.Server{
